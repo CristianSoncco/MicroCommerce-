375785449a18665436dabb801364fcd1d4a7fc50
Cristian Antony Soncco Boza <cristian_soncco@msn.com>
2026-01-18 21:55:57 +0100

    feat: Añadir configuraciones de escaneo de entidades y repositorios JPA en ProductServiceApplication para mejorar la gestión de datos en el servicio de productos.

--- product-service/src/main/java/com/microcommerce/product/ProductServiceApplication.java
+++ product-service/src/main/java/com/microcommerce/product/ProductServiceApplication.java
@@ -2,10 +2,14 @@ package com.microcommerce.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EntityScan(basePackages = "com.microcommerce.product.entity")
+@EnableJpaRepositories(basePackages = "com.microcommerce.product.repository")
 public class ProductServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProductServiceApplication.class, args);
